8dc8cc3b6808941fb5750be734e5855a3212271a
Musab Idris <musabidris15@gmail.com>
2025-12-28 15:21:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-587322716-5bb8b",
+  "appId": "1:352288848431:web:0ad2d8f8b4c5ab26e124da",
+  "apiKey": "AIzaSyDN_RmrN0PeUWhJRsdlldN7bo9ZlzT5ZLU",
+  "authDomain": "studio-587322716-5bb8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "352288848431"
+};
