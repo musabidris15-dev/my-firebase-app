732f819bbba27447ff112be44ea4a9e9187cecdc
Musab Idris <musabidris15@gmail.com>
2026-01-02 15:21:25 +0000

    check codes that are causing port conflicts now the app is using port 90

--- next.config.ts
+++ next.config.ts
@@ -12,10 +12,6 @@ const nextConfig: NextConfig = {
   },
   // This part keeps your connection working
   allowedDevOrigins: [
-    "localhost:3000",
-    "9000-firebase-studio-1761077070485.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev",
-    "9001-firebase-studio-1761077070485.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev",
-    "9002-firebase-studio-1761077070485.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev",
     "9005-firebase-studio-1761077070485.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"
   ],
 };
