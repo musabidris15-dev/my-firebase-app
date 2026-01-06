624c008e37f34e1cef72c44bf5c37307f2c332b6
Musab Idris <musabidris15@gmail.com>
2026-01-06 04:16:39 +0000

    complete secure secret setup with apphosting.yaml

--- src/app/ai/genkit.ts
+++ src/app/ai/genkit.ts
@@ -1,13 +1,10 @@
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
 
-// Hardcoded Key
-const myKey = 'AIzaSyCNJS_aiwaHrwmcp5aUb87ls0eilXIVods'; 
-
 export const ai = genkit({
   plugins: [
     googleAI({ 
-      apiKey: myKey 
+      apiKey: process.env.GEMINI_API_KEY 
     })
   ],
-  });
+});
